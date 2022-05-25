f22280a1675226da8bc8922a7b68cea74deb1ea7
vathunwa007 <vathunwa@gmail.com>
2022-05-25 18:56:37 +0700

    Update mobile and function dynamicIcon

--- src/util/dynamic-icon.js
+++ src/util/dynamic-icon.js
@@ -1,96 +1,135 @@
+var imgicon = {
+  none: "",
+  warning:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Antu_dialog-warning.svg/2048px-Antu_dialog-warning.svg.png",
+  warningGas:
+    "https://cdn0.iconfinder.com/data/icons/hazard-warning-signs-42-high-quality-hazard-symb-1/283/gas_mask-512.png",
+  warningWork:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/OOjs_UI_icon_alert-warning.svg/1200px-OOjs_UI_icon_alert-warning.svg.png",
+  warningWorkIm:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1VIaG7eqsvwCbV2YVYjbqkV9irGB44VuHKQ&usqp=CAU"
+};
+
 function CreateIcon(color = "red", type = false, isdraw = 1) {
-    const drawer = new Promise((resolve, reject) => {
-        var element = document.getElementById("canvas");
-        if (element) {
-            element.remove();
-        }
-        var canvas = document.createElement("canvas");
-        canvas.id = "canvas";
-        canvas.width = 300;
-        canvas.height = 300;
-        var body = document.getElementsByTagName("body")[0];
-        body.appendChild(canvas);
-        var ctx = canvas.getContext("2d");
-        var imgicon = {
-            none: '',
-            warning:
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Antu_dialog-warning.svg/2048px-Antu_dialog-warning.svg.png",
-            warningGas:
-                "https://cdn0.iconfinder.com/data/icons/hazard-warning-signs-42-high-quality-hazard-symb-1/283/gas_mask-512.png",
-            warningWork:
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/OOjs_UI_icon_alert-warning.svg/1200px-OOjs_UI_icon_alert-warning.svg.png",
-            warningWorkIm:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1VIaG7eqsvwCbV2YVYjbqkV9irGB44VuHKQ&usqp=CAU"
-        };
-        var ct = [
-            "source-over", //  0
-            "source-in", //  1
-            "source-out", //  2
-            "source-atop", //  3
-            "destination-over", //  4
-            "destination-in", //  5
-            "destination-out", //  6
-            "destination-atop", //  7
-            "lighter", //  8
-            "darker", //  9
-            "copy", // 10
-            "xor" // 11
-        ];
-        if (type) {
-            const image = new Image();
+  const drawer = new Promise((resolve, reject) => {
+    var element = document.getElementById("canvas");
+    if (element) {
+      element.remove();
+    }
+    var canvas = document.createElement("canvas");
+    canvas.id = "canvas";
+    canvas.width = 210;
+    canvas.height = 210;
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(canvas);
+    var ctx = canvas.getContext("2d");
 
-            image.crossOrigin = "anonymous";
-            image.onload = async () => {
-                await ctx.save();
-                await ctx.beginPath();
-                ctx.globalCompositeOperation = ct[0];
-                ctx.fillStyle = "#FFF";
-                /* ctx.arc(150, 150, 130, 0, Math.PI * 2, false) */
-                await ctx.arc(200, 60, 100 /*50*/, 0, 2 * Math.PI);
-                ctx.strokeStyle = "#FFF";
-                //ctx.stroke()
-                await ctx.clip();
-                //ctx.fill();
-                /* ctx.drawImage(image, 50, -50,300,300) */
-                await ctx.drawImage(image, canvas.width / 2, 0, 100, 110);
-                // ctx.restore();
-                resolve();
-            };
-            image.src = imgicon[type];
-        } else {
-            resolve();
-        }
-        // ctx.beginPath();
-        // ctx.globalCompositeOperation = ct[9];
-        // ctx.fillStyle = "#FFF";
-        // ctx.arc(200, 60, 50, 0, 2 * Math.PI);
-        // ctx.fill();
+    var ct = [
+      "source-over", //  0
+      "source-in", //  1
+      "source-out", //  2
+      "source-atop", //  3
+      "destination-over", //  4
+      "destination-in", //  5
+      "destination-out", //  6
+      "destination-atop", //  7
+      "lighter", //  8
+      "darker", //  9
+      "copy", // 10
+      "xor" // 11
+    ];
+    if (type) {
+      const image = new Image();
 
-        ctx.beginPath();
-        ctx.globalCompositeOperation = ct[4];
-        ctx.fillStyle = color;
-        ctx.strokeStyle = "#000000";
-        if (isdraw == 1) {
-            ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI);
-        } else {
-            ctx.fillRect(20, 20, canvas.width / 1.5, canvas.height / 1.5);
-            ctx.strokeRect(19, 19, canvas.width / 1.49, canvas.height / 1.49);
-        }
-        ctx.fill();
-        ctx.stroke();
+      image.crossOrigin = "anonymous";
+      image.onload = async () => {
+        await ctx.save();
+        await ctx.beginPath();
+        ctx.globalCompositeOperation = ct[0];
+        ctx.fillStyle = "#FFF";
+        /* ctx.arc(150, 150, 130, 0, Math.PI * 2, false) */
+        await ctx.arc(200, 80, 100 /*50*/, 0, 2 * Math.PI);
+        ctx.strokeStyle = "#FFF";
+        //ctx.stroke()
+        await ctx.clip();
+        //ctx.fill();
+        /* ctx.drawImage(image, 50, -50,300,300) */
+        await ctx.drawImage(image, canvas.width / 2, 10, 100, 110);
+        // ctx.restore();
+        resolve();
+      };
+      image.src = imgicon[type];
+    } else {
+      resolve();
+    }
+    // ctx.beginPath();
+    // ctx.globalCompositeOperation = ct[9];
+    // ctx.fillStyle = "#FFF";
+    // ctx.arc(200, 60, 50, 0, 2 * Math.PI);
+    // ctx.fill();
 
-        // ctx.beginPath();
-        // ctx.globalCompositeOperation = ct[4];
-        // ctx.rect(0, 0, 300, 300);
-        // ctx.fillStyle = "#000";
-        // ctx.fill();
-    });
+    ctx.beginPath();
+    ctx.globalCompositeOperation = ct[4];
+    ctx.fillStyle = color;
+    ctx.strokeStyle = "#000000";
+    if (isdraw == 1) {
+      ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI);
+    } else {
+      ctx.fillRect(20, 20, canvas.width, canvas.height);
+      ctx.strokeRect(20, 20, canvas.width, canvas.height);
+    }
+    ctx.fill();
+    ctx.stroke();
 
-    return drawer.then(async () => {
-        var canvas = document.querySelector("#canvas");
-        var dataURL = await canvas.toDataURL("image/png", 2.0);
-        return dataURL;
-    });
+    // ctx.beginPath();
+    // ctx.globalCompositeOperation = ct[4];
+    // ctx.rect(0, 0, 300, 300);
+    // ctx.fillStyle = "#000";
+    // ctx.fill();
+  });
+
+  return drawer.then(async () => {
+    var canvas = document.querySelector("#canvas");
+    var dataURL = await canvas.toDataURL("image/png", 2.0);
+    return dataURL;
+  });
+}
+
+function CreateImgIcon(
+  img = "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
+  type = "warning"
+) {
+  var canvas = document.createElement("canvas");
+  canvas.id = "canvas";
+  canvas.width = 210;
+  canvas.height = 210;
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(canvas);
+  var ctx = canvas.getContext("2d");
+
+  const image = new Image();
+
+  image.crossOrigin = "anonymous";
+  image.onload = async () => {
+    await ctx.save();
+    await ctx.beginPath();
+    ctx.globalCompositeOperation = "source-over";
+    await ctx.arc(200, 80, 100, 0, 2 * Math.PI);
+    await ctx.drawImage(image, canvas.width / 2, 0, 100, 110);
+  };
+  image.src = imgicon[type];
+
+  const image2 = new Image();
+  image2.crossOrigin = "anonymous";
+  image2.onload = async () => {
+    await ctx.save();
+    await ctx.beginPath();
+    ctx.globalCompositeOperation = "destination-over";
+    await ctx.rect(0, 0, 300, 300);
+    await ctx.clip();
+    await ctx.drawImage(image2, 0, 0, canvas.width, canvas.height);
+  };
+  image2.src = img;
 }
 
 async function DownloadIcon(
@@ -108,5 +147,6 @@ async function DownloadIcon(
 
 export {
     CreateIcon,
+    CreateImgIcon,
     DownloadIcon
 }
